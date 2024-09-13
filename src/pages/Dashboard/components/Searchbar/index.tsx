985d1a0d1840bdda601b11a89dd15312fb176045
Daniel Santos <im.dancardoso@gmail.com>
2024-09-13 20:51:14 -0300

    feat: implement refresh button

--- src/pages/Dashboard/components/Searchbar/index.tsx
+++ src/pages/Dashboard/components/Searchbar/index.tsx
@@ -1,24 +1,33 @@
 import { HiRefresh } from "react-icons/hi";
-import { useHistory } from "react-router-dom";
-import Button from "~/components/Buttons";
+import { useNavigate } from "react-router-dom";
+
 import { IconButton } from "~/components/Buttons/IconButton";
-import TextField from "~/components/TextField";
+import Button from "~/components/Buttons";
 import routes from "~/router/routes";
+import TextField from "~/components/TextField";
+
 import * as S from "./styles";
+
 export const SearchBar = () => {
-  const history = useHistory();
+  const navigate = useNavigate();
 
   const goToNewAdmissionPage = () => {
-    history.push(routes.newUser);
+    navigate(routes.newUser);
   };
-  
+
+  const refetchData = () => {
+    navigate(".", { replace: true });
+  };
+
   return (
     <S.Container>
-      <TextField  placeholder="Digite um CPF válido" />
+      <TextField placeholder="Digite um CPF válido" />
+
       <S.Actions>
-        <IconButton aria-label="refetch">
+        <IconButton aria-label="refetch" onClick={refetchData}>
           <HiRefresh />
         </IconButton>
+
         <Button onClick={() => goToNewAdmissionPage()}>Nova Admissão</Button>
       </S.Actions>
     </S.Container>
