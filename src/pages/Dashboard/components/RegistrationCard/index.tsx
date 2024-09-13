2470e45e3794bc17aebbf0f695741f9114a2a57c
Daniel Santos <im.dancardoso@gmail.com>
2024-09-13 20:50:49 -0300

    feat: implement button actions

--- src/pages/Dashboard/components/RegistrationCard/index.tsx
+++ src/pages/Dashboard/components/RegistrationCard/index.tsx
@@ -1,37 +1,89 @@
-import { ButtonSmall } from "~/components/Buttons";
-import * as S from "./styles";
 import {
   HiOutlineMail,
   HiOutlineUser,
   HiOutlineCalendar,
   HiOutlineTrash,
 } from "react-icons/hi";
+import { useRevalidator } from "react-router-dom";
+
+import { ButtonSmall } from "~/components/Buttons";
+import * as Api from "~/services/api";
+
+import * as S from "./styles";
 
 type Props = {
   data: any;
 };
 
 const RegistrationCard = (props: Props) => {
+  const { revalidate } = useRevalidator();
+
+  const handleChangeStatus = (status: string) => {
+    Api.put(`registrations/${props.data.id}`, {
+      ...props.data,
+      status: status,
+    }).finally(() => {
+      revalidate();
+    });
+  };
+
+  const handleDelete = () => {
+    Api.del(`registrations/${props.data.id}`).finally(() => {
+      revalidate();
+    });
+  };
+
   return (
     <S.Card>
       <S.IconAndText>
         <HiOutlineUser />
         <h3>{props.data.employeeName}</h3>
       </S.IconAndText>
+
       <S.IconAndText>
         <HiOutlineMail />
         <p>{props.data.email}</p>
       </S.IconAndText>
+
       <S.IconAndText>
         <HiOutlineCalendar />
         <span>{props.data.admissionDate}</span>
       </S.IconAndText>
-      <S.Actions>
-        <ButtonSmall bgcolor="rgb(255, 145, 154)" >Reprovar</ButtonSmall>
-        <ButtonSmall bgcolor="rgb(155, 229, 155)">Aprovar</ButtonSmall>
-        <ButtonSmall bgcolor="#ff8858">Revisar novamente</ButtonSmall>
 
-        <HiOutlineTrash />
+      <S.Actions>
+        {props.data.status === "REVIEW" ? (
+          <ButtonSmall
+            bgcolor="rgb(155, 229, 155)"
+            onClick={() => handleChangeStatus("APPROVED")}
+          >
+            Aprovar
+          </ButtonSmall>
+        ) : undefined}
+
+        {props.data.status === "REVIEW" ? (
+          <ButtonSmall
+            bgcolor="rgb(255, 145, 154)"
+            onClick={() => handleChangeStatus("REPROVED")}
+          >
+            Reprovar
+          </ButtonSmall>
+        ) : undefined}
+
+        {props.data.status === "APPROVED" ||
+        props.data.status === "REPROVED" ? (
+          <ButtonSmall
+            bgcolor="#ff8858"
+            onClick={() => handleChangeStatus("REVIEW")}
+          >
+            Revisar novamente
+          </ButtonSmall>
+        ) : undefined}
+
+        <HiOutlineTrash
+          onClick={() => {
+            handleDelete();
+          }}
+        />
       </S.Actions>
     </S.Card>
   );
